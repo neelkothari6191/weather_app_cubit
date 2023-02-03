fb254a42bf40fff8865cf06ee1367769aab8c8b7
Neel Kothari <neelkothariak@gmail.com>
2023-02-03 17:36:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_app/bloc/weather_bloc.dart';
+import 'package:flutter_weather_app/cubit/weather_cubit.dart';
+import 'package:flutter_weather_app/data/weather_repository.dart';
+import 'package:flutter_weather_app/pages/weather_search_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: BlocProvider(
+        create: (context) => WeatherBloc(FakeWeatherRepository()),
+        child: WeatherSearchPage(),
+      ),
+    );
+  }
+}
